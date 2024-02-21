b3ffc490f488f55340afdefadc1ffcdc6bcaed00
Morgbn <morgan.balde@gmail.com>
2024-02-21 19:37:27 +0800

    fix: :label: add types to $csrfFetch
    Resolves #19

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,12 +1,22 @@
 import { $fetch } from 'ofetch'
-import type { FetchOptions } from 'ofetch'
+import type { FetchOptions, FetchRequest, $Fetch } from 'ofetch'
 import { defineNuxtPlugin, useCsrf } from '#imports'
+
+// types copied from ofetch (not exposed by oftech)
+interface ResponseMap {
+    blob: Blob;
+    text: string;
+    arrayBuffer: ArrayBuffer;
+    stream: ReadableStream<Uint8Array>;
+}
+type ResponseType = keyof ResponseMap | "json";
+type MappedType<R extends ResponseType, JsonType = any> = R extends keyof ResponseMap ? ResponseMap[R] : JsonType;
 
 export default defineNuxtPlugin(() => {
   const { csrf } = useCsrf()
   return {
     provide: {
-      csrfFetch: (request: string, options?: FetchOptions, fetch = $fetch) => {
+      csrfFetch: <T = any, R extends ResponseType = "json"> (request: FetchRequest, options?: FetchOptions<R>, fetch: $Fetch = $fetch): Promise<MappedType<R, T>> => {
         if (!options) { options = {} }
         options.headers = (options.headers || {}) as Record<string, string>
         options.headers['csrf-token'] = csrf
