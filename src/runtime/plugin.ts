87ad5fafd85312086f454577c8925e2b8f13f73c
Morgbn <morgan.balde@gmail.com>
2024-02-22 14:17:32 +0800

    fix: :label: declare plugin type

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -12,16 +12,29 @@ interface ResponseMap {
 type ResponseType = keyof ResponseMap | "json";
 type MappedType<R extends ResponseType, JsonType = any> = R extends keyof ResponseMap ? ResponseMap[R] : JsonType;
 
+type $CsrfFetch = <T = any, R extends ResponseType = "json">(request: FetchRequest, options?: FetchOptions<R>, fetch?: $Fetch) => Promise<MappedType<R, T>>
+
 export default defineNuxtPlugin(() => {
   const { csrf } = useCsrf()
+  const csrfFetch: $CsrfFetch = (request, options, fetch = $fetch) => {
+    if (!options) { options = {} }
+    options.headers = (options.headers || {}) as Record<string, string>
+    options.headers['csrf-token'] = csrf
+    return fetch(request, options)
+  }
   return {
-    provide: {
-      csrfFetch: <T = any, R extends ResponseType = "json"> (request: FetchRequest, options?: FetchOptions<R>, fetch: $Fetch = $fetch): Promise<MappedType<R, T>> => {
-        if (!options) { options = {} }
-        options.headers = (options.headers || {}) as Record<string, string>
-        options.headers['csrf-token'] = csrf
-        return fetch(request, options)
-      }
-    }
+    provide: { csrfFetch }
   }
 })
+
+declare module '#app' {
+  interface NuxtApp {
+    $csrfFetch: $CsrfFetch
+  }
+}
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $csrfFetch: $CsrfFetch
+  }
+}
