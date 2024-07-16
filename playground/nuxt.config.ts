c48435eadf880fbff5c238c6f830488d85599c3e
Morgbn <morgan.balde@gmail.com>
2024-07-16 16:32:59 +0200

    docs: :memo: add nitro.prerender in playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -5,13 +5,16 @@ export default defineNuxtConfig({
   modules: [
     module
   ],
+  nitro: {
+    prerender: { routes: ['/'] }
+  },
   routeRules: {
     '/api/nocsrf': {
       csurf: false
     }
   },
   csurf: {
-    https: false,
+    https: process.env.NODE_ENV === 'production',
     methodsToProtect: ['POST']
   }
 })
